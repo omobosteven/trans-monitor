3d1ceca36b5f7c758a35ddd4090235f490a6201d
omobosteven <omobosteven@gmail.com>
2020-08-21 23:34:30 +0100

    ch(setup): setup react application - parcel bundler - eslint - prettier

---
+++ src/app.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { render } from 'react-dom';
+
+const App = () => {
+  return (
+    <div>
+      <h1>Welcome to Trans Monitor!!!</h1>
+    </div>
+  );
+};
+
+render(<App />, document.getElementById('root'));
